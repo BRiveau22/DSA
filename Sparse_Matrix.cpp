bc720874a05f49fd2feff8ffe0f6826390a81672
BRiveau <briveau@uri.edu>
2023-03-10 13:00:22 -0500

    Added comments to Sparse_Matrix.cpp to aid in method implementations.

--- Sparse_Matrix.cpp
+++ Sparse_Matrix.cpp
@@ -20,7 +20,27 @@ Sparse_Matrix::Sparse_Matrix(std::vector<std::vector<int>> matrix) {
 		}
 	}
 
+	//Sets the tail as well as updates prev and next for tail and head
 	this->tail = current_node->prev;
 	this->head->prev = this->tail;
 	this->tail->next = this->head;
+}
+
+Sparse_Matrix Sparse_Matrix::multiply(Sparse_Matrix mult_matrix) {
+	//Multiplies the current matrix by mult_matrix
+}
+
+Sparse_Matrix Sparse_Matrix::add(Sparse_Matrix add_matrix) {
+	//Adds the current matrix with add_matrix
+}
+
+
+Sparse_Matrix Sparse_Matrix::insert_row(int row, std::vector<int> new_row) {
+	//Inserts a new row at the specified index (with indexes < 0 prepending 
+	//the row, and indexes > num_rows appending the row)
+}
+
+Sparse_Matrix Sparse_Matrix::insert_col(int col, std::vector<int> new_col) {
+	//Inserts a new col at the specified index (with indexes < 0 prepending 
+	//the col, and indexes > col appending the row)
 }
