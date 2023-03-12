3fead570c400cd92cb72f3f6c5e34cdb5a231191
BRiveau <briveau@uri.edu>
2023-03-12 09:28:03 -0400

    Added pseudo code for Sparse_Matrix methods.

--- Sparse_Matrix.cpp
+++ Sparse_Matrix.cpp
@@ -28,19 +28,40 @@ Sparse_Matrix::Sparse_Matrix(std::vector<std::vector<int>> matrix) {
 
 Sparse_Matrix Sparse_Matrix::multiply(Sparse_Matrix mult_matrix) {
 	//Multiplies the current matrix by mult_matrix
+	//Determine whether or not mult_matrix has the same number of columns as this->matrix has rows
+	//If there is a difference, assume adding zeroes to the matrix to make them match
+
+	//Multiply every value in each row by every value in each column
+	//Take the summation of the multiplied values for the given row and column
 }
 
 Sparse_Matrix Sparse_Matrix::add(Sparse_Matrix add_matrix) {
 	//Adds the current matrix with add_matrix
+	//Checks if the two matrices are the same order (size)
+	//If there is a difference, assume adding zeroes to the matrix to make them match
+
+	//Add the two values at the corresponding locations
 }
 
 
 Sparse_Matrix Sparse_Matrix::insert_row(int row, std::vector<int> new_row) {
 	//Inserts a new row at the specified index (with indexes < 0 prepending 
 	//the row, and indexes > num_rows appending the row)
+	
+	//Create node pointers for every value in new_row
+	//Switch pointers from row before insertion index to the start value of new_row
+	//Switch pointers from new_row to next_row
+
 }
 
 Sparse_Matrix Sparse_Matrix::insert_col(int col, std::vector<int> new_col) {
 	//Inserts a new col at the specified index (with indexes < 0 prepending 
 	//the col, and indexes > col appending the row)
+
+	//Create node pointers for every value in new_col
+	//Loop through every value in the Sparse_Matrix
+	//When current_col is less than the insertion index, insert first value
+	//Point to new value
+	//Point from new value to next value
+	//Repeat until all columns are filled
 }
